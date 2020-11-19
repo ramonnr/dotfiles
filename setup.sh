b2ec2331f694e4ff582ac0b65b194a6ee798adde
Ramón N.Rodriguez <ramonnr@kth.se>
2020-11-19 17:30:00 +0100

    Added alacritty nord settings and changed error handling in setup

--- setup.sh
+++ setup.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env sh
 
-# param: echo this string before exiting
-fail(){
-    echo $1
-    exit 1
-}
-
+set -ex
 echo "this will symlink things all over your home, hope you like it"
 
-ln -sf $(pwd)/.tmux.conf ~/ || fail "tmux.conf failed"
+ln -sf $(pwd)/.tmux.conf ~/
 
 mkdir -p ~/.config/nvim
-ln -sf $(pwd)/nvim/init.vim ~/.config/nvim/ || fail "init.vim failed"
-ln -sf $(pwd)/nvim/plug-config/ ~/.config/nvim/ || fail "plug-config failed"
-
+ln -sf $(pwd)/nvim/init.vim ~/.config/nvim/
+ln -sf $(pwd)/nvim/plug-config/ ~/.config/nvim/
+mkdir -p ~/.config/alacritty
+ln -sf $(pwd)/alacritty.yml ~/.config/alacritty/
